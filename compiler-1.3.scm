fa073cb574e74f4c298cf22ff87da5dcb41798d4
martin823 <martin823@126.com>
2015-02-12 21:38:41 +0800

    Update compiler-1.3.scm

--- compiler-1.3.scm
+++ compiler-1.3.scm
@@ -7,21 +7,33 @@
         (length ’(arg* ...)))
       (putprop ’prim-name ’*emitter*
         (lambda (arg* ...) b b* ...)))]))
+        
+        
 (define (primitive? x)
   (and (symbol? x) (getprop x ’*is-prim*)))
+  
+  
 (define (primitive-emitter x)
   (or (getprop x ’*emitter*) (error ---)))
+  
+  
 (define (primcall? expr)
   (and (pair? expr) (primitive? (car expr))))
+  
+  
 (define (emit-primcall expr)
   (let ([prim (car expr)] [args (cdr expr)])
     (check-primcall-args prim args)
     (apply (primitive-emitter prim) args)))
+    
+    
 (define (emit-expr expr)
   (cond
    [(immediate? expr) (emit-immediate expr)]
    [(primcall? expr)  (emit-primcall expr)]
    [else (error ---)]))
+   
+   
 (define (emit-program expr)
   (emit-function-header "scheme_entry")
   (emit-expr expr)
